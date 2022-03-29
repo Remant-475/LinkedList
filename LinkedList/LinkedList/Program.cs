bc924d25ace288de862a64f7ac367c4ebd74f407
Remant <remantmahato9798@gmail.com>
2022-03-29 07:45:09 +0530

    Added : UC1-CreateLinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main()
+        {
+            Linkedlist linkedlist = new Linkedlist();
+            Node node1 = new Node(23);
+            Node node2 = new Node(24);
+            Node node3 = new Node(25);
+
+        }
+    }
+}
---
+++ LinkedList/LinkedList/linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Linkedlist
+    {
+        public Node Head;
+        public Node Tail;
+
+        public Linkedlist()
+        {
+            Head = null;
+            Tail = null;
+        }
+    }
+}
